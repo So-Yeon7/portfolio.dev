bcda28381e62c57b5c08043359e3e958c5ed7c4e
So-Yeon7 <viktoria17086@gmail.com>
2025-04-19 12:09:25 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const ids = ['About', 'Projects', 'Contacts'];
+    ids.forEach(id=> {
+        const element= document.getElementById(id);
+        if (element){
+            element.style.display='none';//Скрываем текст
+        }
+    });
+
+
+
+    const infoToggleBtns = document.getElementsByClassName('infoToggleBtns');
+    for (let i = 0; i < infoToggleBtns.length; i++){
+        infoToggleBtns[i].addEventListener('click', 
+            (e)=>{
+                const blockName = e.target.name;
+                const infoBlock = document.getElementById(blockName);
+                if (infoBlock.style.display == 'none'){
+                    infoBlock.style.display = 'block';
+                }
+                else {
+                    infoBlock.style.display = 'none';
+                }
+            }
+        );
+    }
+});
+
+
+
+
+
+function displayPlot(){
+    console.log("Plot function is called");
+    
+    // const ids=['intro'];
+
+    // ids.forEach(id=>{
+    //     const element= document.getElementById(id);
+    //     if(element){
+    //         if(element.style.display==="none") {
+    //             element.style.display="block";
+    //             //Показываем элемент element.style.paddingTop="15px";
+    //         }
+    //         else{
+    //             element.style.display="none";
+    //             //Скрываем элемент
+    //         }
+    //     }
+    // });
+//Перенаправление window.location.href='#About';
+}
+
